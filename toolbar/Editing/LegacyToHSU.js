91246197fea77dd10ec52800c4f091029d298588
Joshua David <Joshua@Joshua-David.net>
2014-07-31 16:24:27 -0700

    Copy ALL settings from Legacy to HSU forums

--- toolbar/Editing/LegacyToHSU.js
+++ toolbar/Editing/LegacyToHSU.js
@@ -13,57 +13,84 @@ var options = {
 var legToHSUBtn = new FunctionButton(title, action, visFn, options);
 TM.add(legToHSUBtn);
 
-function replaceOneLegacyWithHSU(id) {
-    $.get('/course/modedit.php?update=' + id).then(function(response) {
-        var legacySettings = $(response).find('form').serializeObject();
-        var section = legacySettings['section'];
-        var title = legacySettings['name'];
+function replaceOneLegacyWithHSU(legid) {
+    var legacySettings, hsuSettings, section, title, courseId, addurl,
+        $leg, $hsu, hsuid, $icon, moveParams;
+    $.get('/course/modedit.php?update=' + legid).then(function(response) {
+        legacySettings = $(response).find('form').serializeObject();
+        section = legacySettings['section'];
+        title = legacySettings['name'];
         if(title.match(/News Forum/gi) || title.match(/Announcements/gi)) {
+            var d = $.Deferred();
+            var p = d.promise();
+            // Just never resolve. There's probably a better way of doing
+            // this though.
+            d.reject("Do not convert News Forum.");
+            return p;
+        }
+        courseId = getCourseId();
+        addurl = '/course/modedit.php?add=hsuforum&course=' + courseId;
+        addurl += '&section=' + section;
+        return $.get(addurl);
+    }).then(function(response) {
+        hsuSettings = $(response).find('form').serializeObject();
+        for(var key in legacySettings) {
+            if(hsuSettings[key] == "") {
+                hsuSettings[key] = legacySettings[key];
+            } else if(legacySettings[key] == legid) {
+            } else if(key == "modulename" || key == "module") {
+            } else if(hsuSettings[key] == legacySettings[key]) {
+            } else if(hsuSettings[key] !== undefined) {
+                hsuSettings[key] = legacySettings[key];
+            } else {
+            }
+            hsuSettings["add"] = "hsuforum";
+        }
+        return $.post(addurl, hsuSettings);
+    }).then(function(response) {
+        $hsu = findActivityInHtml(response, section, "hsuforum", title); 
+        if($hsu === null) {
+            console.log($(response).find('body')[0]);
             return;
         }
-        var courseId = getCourseId();
-        $.get('/course/modedit.php?add=hsuforum&course=' + courseId + '&section=' + section)
-        .then(function(response) {
-            var hsuSettings = $(response).find('form').serializeObject();
-            for(var key in legacySettings) {
-                if(hsuSettings[key] === "") {
-                    hsuSettings[key] = legacySettings[key];
-                }
-            }
-            $.post('/course/modedit.php', hsuSettings).then(function(response) {
-                var $hsu = findActivityInHtml(response, section, "hsuforum", title); 
-                if($hsu === null) {
-                    return;
-                }
-                var hsuid = $hsu.find('.activityinstance a').attr('href')
-                .match(/id=(\d+)/)[1];
-                $.get('/course/mod.php?hide=' + id + '&sesskey=' + getSesskey()); 
-                var params = { 
-                    sesskey: getSesskey(),
-                    courseId: courseId,
-                    class: "resource",
-                    field: "move",
-                    id: hsuid,
-                    sectionId: section,
-                    beforeId: id
-                };
-                $.post('/course/rest.php', params).then(function() {
-                    var $leg = $('#module-' + id);
-                    var $hsu = $leg.clone();
-                    $hsu.insertBefore($leg);
-                    $hsu.attr('id', 'module-' + hsuid);
-                    var $icon = $hsu.find('img.activityicon');
-                    $icon.attr('src', $icon.attr('src').replace("forum", "hsuforum"));
-                    $hsu.find('a').each(function() {
-                        this.href = this.href.toString().replace(id, hsuid);
-                    });
-                    $leg.find('.activityinstance a').addClass('dimmed');
-                    $leg.find('.editing_hide img').each(function() {
-                        this.src = this.src.toString().replace('hide', 'show');
-                    });
-                });
-            })
+        hsuid = $hsu.find('.activityinstance a').attr('href')
+            .match(/id=(\d+)/)[1];
+        $('#section-' + section + 'ul').append($hsu);
+        $.get('/course/mod.php?hide=' + legid + '&sesskey=' + getSesskey()); 
+        return moveModuleBefore(hsuid, legid, section);
+    }).then(function() {
+        $leg = $('#module-' + legid);
+        $hsu = $leg.clone();
+        $hsu.insertBefore($leg);
+        $hsu.attr('id', 'module-' + hsuid);
+        $icon = $hsu.find('img.activityicon');
+        $icon.attr('src', $icon.attr('src').replace("forum", "hsuforum"));
+        $hsu.find('a').each(function() {
+            this.href = this.href.toString().replace(legid, hsuid);
         });
+        $leg.find('.activityinstance a').addClass('dimmed');
+        $leg.find('.editing_hide img').each(function() {
+            this.src = this.src.toString().replace('hide', 'show');
+        });
+    });
+}
+
+function moveModuleBefore(module_id, before_id, section) {
+    var moveParams = { 
+        sesskey: getSesskey(),
+        courseId: getCourseId(),
+        class: "resource",
+        field: "move",
+        id: module_id,
+        sectionId: section,
+        beforeId: before_id
+    };
+    return $.post('/course/rest.php', moveParams).then(function() {
+        var d = $.Deferred();
+        var p = d.promise();
+        $('#module-' + module_id).insertBefore('#module-' + before_id);
+        d.resolve();
+        return p;
     });
 }
 
@@ -73,5 +100,18 @@ function replaceAllLegacyWithHSU() {
         var href = $forum.find('.activityinstance a').attr('href');
         var id = href.match(/id=(\d+)/)[1];
         replaceOneLegacyWithHSU(id);
+    });
+}
+
+function deleteAllHSU() {
+    $('.activity.modtype_hsuforum').each(function() {
+        var $module = $(this);
+        var id = $module.attr('id').match(/module-(\d+)/)[1];
+        var url = '/course/mod.php';
+        url += '?delete=' + id;
+        url += '&confirm=1';
+        url += '&sr=0';
+        url += '&sesskey=' + getSesskey();
+        $.get(url).then(function() { $module.remove() });
     });
 }
