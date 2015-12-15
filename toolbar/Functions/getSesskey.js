7a6e559abace064b265a2a29e09adcb2480a174d
simongrigorian <simongrigorian@yahoo.com>
2015-12-15 15:47:31 -0800

    added console log

--- toolbar/Functions/getSesskey.js
+++ toolbar/Functions/getSesskey.js
@@ -4,5 +4,6 @@ function getSesskey() {
     var $linkWithSesskey = $('a[href*="sesskey"]');
     var href = $linkWithSesskey.attr('href');
     var sesskey = href.match(/sesskey=([\w\d]+)/)[1];
+	console.log(sesskey);
     return sesskey;
 }
