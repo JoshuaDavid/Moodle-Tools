9049b8e181c5ffbb89a298da5b1f039332eaf496
simongrigorian <simongrigorian@yahoo.com>
2015-12-22 15:25:41 -0800

    added another checkid here

--- toolbar/Functions/getCourseId.js
+++ toolbar/Functions/getCourseId.js
@@ -5,11 +5,13 @@ function getCourseId() {
    // Grab the course id from the address bar in the current page
    // the match function returns an array of string values
    // the second index is the int value that we need
-   // console.log(window.location.href); //debug line
-   /*
-	if(window.location.href.match(/id=(\d+)/) == null)
+	
+	/*if there is no session id, just stop the toolbar.
+	 This eliminated the bug in firefox pooping out code in the html code
+ 	*/
+	if(window.location.href == null || window.location.href.match(/id=(\d+)/) == null)
 		return;
-	*/
+	/*else continue buildin the toolbar*/
 	
     var courseid = window.location.href.match(/id=(\d+)/)[1];
 	
