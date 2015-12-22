5df5a35b39816a60a235bbfce70c35f503a500c1
simongrigorian <simongrigorian@yahoo.com>
2015-12-22 14:56:35 -0800

    console log added before it breaks

--- toolbar/concatjs.php
+++ toolbar/concatjs.php
@@ -1,12 +1,13 @@
 <?php
 //header("Content-type: application/javascript");
 header("Content-Type: text/javascript; charset=UTF-8");
-/*
+
 $id = $_GET['id'];
+console.log($id);
 if(empty($id)) 
 {
 	return;
-}*/
+}
 
 function ls_r_js($path) {
     $fullpaths = array();
