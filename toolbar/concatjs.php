e603c118a62f26ca3cb2b2cc7fe0f82307c161c1
simongrigorian <simongrigorian@yahoo.com>
2015-12-22 12:25:23 -0800

    added php code in an html body

--- toolbar/concatjs.php
+++ toolbar/concatjs.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html PUBLIC"-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+<meta content="utf-8" http-equiv="encoding">
+</head>
+<body>
+
 <?php
 //header("Content-type: application/javascript");
 
@@ -57,3 +66,6 @@ for($i = 0; $i < sizeof($scripts); $i++) {
     }
 }
 ?>
+
+</body>
+</html>
