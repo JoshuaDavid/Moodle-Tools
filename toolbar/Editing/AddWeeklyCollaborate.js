7adea64fe402b8a9d60e2fd34729e1c466a3081a
simongrigorian <simongrigorian@yahoo.com>
2015-12-22 16:19:39 -0800

    same thing

--- toolbar/Editing/AddWeeklyCollaborate.js
+++ toolbar/Editing/AddWeeklyCollaborate.js
@@ -68,7 +68,7 @@ function createCollabForm($form, settings) {
             '<b>Session for [[SECTION]] ([[DATE]])</b> would become ' +
             '<b>Session for Topic 1 (Tuesday, September 2, 2:15pm)</b>' +
             ' (or whatever the correct date is) automatically -- you don\'t have to' +
-            ' enter the dates or section names yourself in the title.' + '<br/><b>Add ([[DATE]]) or [[SECTION]] if you need them</b>' +
+            ' enter the dates or section names yourself in the title.' + '<br/><br/><b>Add ([[DATE]]) or [[SECTION]] if you need them</b>' +
 			'<br/><br/><center><b>Current title: Week 1 - Collaborate Session ... Week 2 - Collaborate Session ... etc<b></center>' +
             '</div>');
     $form.prepend($description);
