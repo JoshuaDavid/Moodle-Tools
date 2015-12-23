8535d5fb76b55e1f269b8ab8ae90225a7527ade3
simongrigorian <simongrigorian@yahoo.com>
2015-12-22 16:16:52 -0800

    same thing.. just fixed some text up in collaborate session description

--- toolbar/Editing/AddWeeklyCollaborate.js
+++ toolbar/Editing/AddWeeklyCollaborate.js
@@ -68,7 +68,8 @@ function createCollabForm($form, settings) {
             '<b>Session for [[SECTION]] ([[DATE]])</b> would become ' +
             '<b>Session for Topic 1 (Tuesday, September 2, 2:15pm)</b>' +
             ' (or whatever the correct date is) automatically -- you don\'t have to' +
-            ' enter the dates or section names yourself in the title.' + '<br/><b>Add ([[DATE]]) or [[SECTION]] if you need it them' +
+            ' enter the dates or section names yourself in the title.' + '<br/><b>Add ([[DATE]]) or [[SECTION]] if you need them' +
+			'<br>Current title: Week 1 - Collaborate Session ... Week 2 - Collaborate Session ... etc' +
             '</div>');
     $form.prepend($description);
     $form.find('input[name="section"]').remove();
