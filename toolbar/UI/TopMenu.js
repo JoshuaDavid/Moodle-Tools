0c56c7a5ee0d9162e423af6c760898caf02d2c4d
simongrigorian <simongrigorian@yahoo.com>
2016-01-11 14:06:21 -0800

    removed some code

--- toolbar/UI/TopMenu.js
+++ toolbar/UI/TopMenu.js
@@ -5,8 +5,9 @@ TopMenu = function() {
 	 This eliminated the bug in firefox pooping out code in the html code
  	*/
 	
-	if(window.location.href == null || window.location.href.match(/id=(\d+)/) == null || window.location.href.match(/notifyeditingon=(\d+)/) == null)
+	if(window.location.href == null || window.location.href.match(/id=(\d+)/) == null)
 		return;
+		 //|| window.location.href.match(/notifyeditingon=(\d+)/) == null
 
 /*	
 	if(window.location.href == null || window.location.href.match(/id=(\d+)/) == null)
